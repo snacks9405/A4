4f265310ee051de56b44845a1665173d72fd1e41
snacks9405 <93486382+snacks9405@users.noreply.github.com>
2021-11-16 17:50:45 -0600

    *chef kiss
    ready for last look

--- A4.java
+++ A4.java
@@ -10,28 +10,24 @@ import javafx.stage.Stage;
 import javafx.scene.shape.Circle;
 import javafx.geometry.*;
 import java.util.Random;
-
-import java.awt.event.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
-
-import java.util.concurrent.TimeUnit;
-
 /**
- * Write a description of JavaFX class A4 here.
+ * an interactive game of C4
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author michael hulbert
+ *         alex rodriguez
+ * @version 11.26.21
  */
 public class A4 extends Application
 {
     // We keep track of the count, and label displaying the count:
     private int[][] board = new int[7][6];
-
     private static int padding = 25;
     private static int cellSize = 50;
     private Label displayMessage;
     private boolean gameover = false;
+
     /**
      * The start method is the main entry point for every JavaFX application. 
      * It is called after the init() method has returned and after 
@@ -42,8 +38,6 @@ public class A4 extends Application
     @Override
     public void start(Stage stage)
     {
-
-        // Create a new grid pane
         GridPane pane = new GridPane();
         pane.setHgap(padding);
         pane.setVgap(padding);
@@ -59,36 +53,35 @@ public class A4 extends Application
                 board[i][j] = 0;
                 Token token = new Token(i, j);
                 tokens[i][j] = token;
-                //pane.add(t, i, j, 1, 1);
                 pane.add(token, i, j);
+                
                 token.setOnMousePressed(event -> {
                         if (!gameover)
                         {
                             int col = token.col;
                             int row;
                             // PLAY PLAYERS MOVE
-                            if((row = token.playTokenColor(tokens, 1, Color.YELLOW)) == -1)
+                            if((row = token.playToken(tokens, 1, Color.YELLOW)) == -1)
                             {
                                 displayMessage.setText("The column is full. Try again!");
                                 return;
                             }
                             // Test victory / tie (player)
                             int playerVictory = tokens[col][row].isVictory();
-                            if(token.applyContinueText(playerVictory, "", "You won!!", "You tied!!", ""))
+                            if(token.updateLabel(playerVictory, "", "You won!!", "You tied!!", ""))
                                 return;
-                                
-                            
+
                             // PLAY COMPUTER'S MOVE
                             do
                             {
                                 col = rand.nextInt(board.length);
-                            } while((row = tokens[col][0].playTokenColor(tokens, 2, Color.RED)) == -1);
-                            
+                            } while((row = tokens[col][0].playToken(tokens, 2, Color.RED)) == -1);
+
                             // Test victory / tie (computer)
                             int compVictory = tokens[col][row].isVictory();
-                            if(tokens[col][row].applyContinueText(compVictory,
+                            if(tokens[col][row].updateLabel(compVictory,
                                 "The computer played in column " + (col+1),
-                                " and won",
+                                " and won!",
                                 " and tied!",
                                 ". Your turn!"))
                                 return;
@@ -103,48 +96,54 @@ public class A4 extends Application
         }
         pane.setStyle("-fx-background-color: blue; -fx-margin: 50%;");
 
-        displayMessage = new Label("It's your turn to play...");
+        displayMessage = new Label("It's your turn to play...");        //initial display message & style & alignment
         displayMessage.setFont(Font.font("Comic Sans MS", 18));
         displayMessage.setStyle("-fx-text-fill: aqua;");
-
         pane.setHalignment(displayMessage, HPos.CENTER);
         pane.add(displayMessage, 0, board[0].length + 1, board.length, 1);
 
         int w = (cellSize+padding) * board.length + padding;
-        int h = (int)((cellSize+padding) * (board[0].length + .5))    + padding;
+        int h = (int)((cellSize + padding) * (board[0].length + .5)) + padding;
         h += cellSize;
-        Scene scene = new Scene(pane, w, h, Color.BLUE);
+        Scene scene = new Scene(pane, w, h);
         stage.setTitle("Connect 4 Game");
         stage.setScene(scene);
 
         stage.show();
-    }
+    }// start method
 
     private class Token extends Circle
     {
         int row, col;
+        
+        /**
+         * Constructor for objects of class Token
+         * 
+         * @param col       col index of token
+         * @param row       row index of token
+         */
         public Token (int col, int row)
         {
-            super((cellSize)/2);
+            super((cellSize) / 2);
             this.row = row;
             this.col = col;
 
             setFill(Color.WHITE);
-        }
-        
-        /*
-         * Applys text to the main label based on the state and parameters
+        }// constructor
+
+        /**
+         * Updates label based on the state and parameters
          * 
-         * @param victoryState   the state of victory
-         * @param prefix         first part of message, always displayed.
-         * @param victoryText    tail of the message, displayed on victory
-         * @param tieText        tail of the message, displayed on tie
-         * @param defaultText    tail of the message, displayed on no-victory
+         * @param victoryState      the state of victory
+         * @param prefix            first part of message, always displayed.
+         * @param victoryText       tail of the message, displayed on victory
+         * @param tieText           tail of the message, displayed on tie
+         * @param defaultText       tail of the message, displayed on no-victory
          * 
          * @return true if the game is over (based on victoryState)
          * 
          */
-        public boolean applyContinueText(int victoryState, String prefix, String victoryText, String tieText, String defaultText)
+        public boolean updateLabel(int victoryState, String prefix, String victoryText, String tieText, String defaultText)
         {
             String message = prefix;
             switch(victoryState)
@@ -160,24 +159,24 @@ public class A4 extends Application
                     break;
             }
             displayMessage.setText(message);
-            
+
             gameover = victoryState != 0;
             return gameover;
-        }
-        
-        /*
-         * Plays a token
+        }// updateLabel method
+
+        /**
+         * Plays a red or yellow token
          * 
-         * @param tokens   an array filled with all token objects (for color manipulation)
-         * @param id       the id of the token user
-         * @param color    the color that will be set
+         * @param tokens    an array filled with all token objects (for color manipulation)
+         * @param id        the id of the token user
+         * @param color     the color that will be set
          * 
          * @return the column the token was played (-1 if invalid move)
          */
-        public int playTokenColor(Token[][] tokens, int id, Color color)
+        public int playToken(Token[][] tokens, int id, Color color)
         {
-            
-            for(int k=board[col].length-1; k>=0; --k)
+
+            for(int k = board[col].length-1; k>=0; --k)
             {
                 if(board[col][k] == 0)
                 {
@@ -187,10 +186,9 @@ public class A4 extends Application
                 }
             }
             return -1;
-        }
-        
-        
-        /*
+        }// playToken method
+
+        /**
          * Tests if a this cell contains a victory condition
          * 
          * @return 0 if no victory, 1 if victory, 2 if tie
@@ -199,39 +197,38 @@ public class A4 extends Application
         {
             int id = board[col][row];
             if(id == 0) return 0;
-                            
+
             boolean victory = isVictoryInDirection(id, 1, 0)   // horizontal
-                || isVictoryInDirection(id, 0, 1)   // vertical
-                || isVictoryInDirection(id, 1, 1)   // diagnol, up
-                || isVictoryInDirection(id, 1, -1);  // diagnol, down
+                || isVictoryInDirection(id, 0,  1)   // vertical
+                || isVictoryInDirection(id, 1,  1)   // diagonal, up
+                || isVictoryInDirection(id, 1, -1);  // diagonal, down
             if(victory) return 1;
-            
+
             boolean tie = true;
-            for(int i=0; i<board.length; i++)
+            for(int i = 0; i < board.length; i++)
                 if(board[i][0] == 0) tie = false;
             return tie ? 2 : 0;
-        }
-        
-        /*
+        }// isVictory method
+
+        /**
          * Helper function for testing victory state
          * 
-         * @param id   the id to test against (1 for player, 2 for computer)
-         * @param dx   delta x: direction to test victory state
-         * @param dy   delta y: direction to test victory state
+         * @param id    the id to test against (1 for player, 2 for computer)
+         * @param dx    delta x: direction to test victory state
+         * @param dy    delta y: direction to test victory state
          * 
          * @return if there is a victory in the given direction
          */
         private boolean isVictoryInDirection(int id, int dx, int dy)
         {
-            for(int i=0; i<4; i++)
+            for(int i = 0; i < 4; i++)
             {
                 boolean victory = true;
-                for(int j=0; j<4; j++)
+                for(int j = 0; j < 4; j++)
                 {
                     int x = col + (j - i) * dx;
                     int y = row + (j - i) * dy;
-                    
-                    
+
                     if(!isPositionValid(x, y) || board[x][y] != id)
                     {
                         victory = false;
@@ -240,11 +237,11 @@ public class A4 extends Application
                 }
                 if(victory) return true;
             }
-            
+
             return false;
-        }
-        
-        /*
+        }// isVictoryInDirection method
+
+        /**
          * Tests if a position is within the bounds of the board
          * 
          * @return if the position is valid
@@ -252,6 +249,6 @@ public class A4 extends Application
         private boolean isPositionValid(int x, int y)
         {
             return x >= 0 && x < board.length && y >= 0 && y < board[x].length;
-        }
-    }
-}
+        }// isPositionValid method
+    }// Token Class ( Inner )
+}// A4 Class
