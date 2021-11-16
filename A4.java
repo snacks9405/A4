1e9e0978368bfb712b28428a2c73fbd954e9aeda
snacks9405 <93486382+snacks9405@users.noreply.github.com>
2021-11-16 11:57:34 -0600

    still gross but more functions
    +game over bool
    +tie game
    +displayM updates
    need:
       4 in a row check
       final clean up

--- A4.java
+++ A4.java
@@ -13,6 +13,7 @@ import java.util.Random;
 
 import java.awt.event.MouseEvent;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 
 /**
  * Write a description of JavaFX class A4 here.
@@ -24,10 +25,11 @@ public class A4 extends Application
 {
     // We keep track of the count, and label displaying the count:
     private int[][] board = new int[7][6];
-    
+
     private static int padding = 25;
     private static int cellSize = 50;
     private Label displayMessage;
+    boolean gameOver = false;
     /**
      * The start method is the main entry point for every JavaFX application. 
      * It is called after the init() method has returned and after 
@@ -44,8 +46,7 @@ public class A4 extends Application
         pane.setHgap(padding);
         pane.setVgap(padding);
         pane.setPadding(new Insets(padding));
-        
-        
+
         Token[][] tokens = new Token[7][6];
         Random rand = new Random();
         for (int i = 0; i < board.length; i++) {
@@ -54,38 +55,59 @@ public class A4 extends Application
                 board[i][j] = 0;
                 Token t = new Token(i, j);
                 tokens[i][j] = t;
-                pane.add(t, i, j, 1, 1);
+                //pane.add(t, i, j, 1, 1);
+                pane.add(t, i, j);
                 t.setOnMousePressed(event -> {
-                    // NOT VALID MOVE
-                    if(board[t.row][0] != 0) return;
-                    // PLAY PLAYERS MOVE
-                    for(int k=board[t.row].length-1; k>=0; --k)
-                    {
-                        if(board[t.row][k] == 0)
-                        {
-                            tokens[t.row][k].setFill(Color.YELLOW);
-                            board[t.row][k] = 1;
-                            break;
-                        }
-                    }
-                    // PLAY COMPUTER'S MOVE
-                    int row;
-                    boolean valid = false;
-                    do
-                    {
-                        row = rand.nextInt(board.length);
-                        for(int k=board[row].length-1; k>=0; --k)
-                        {
-                            if(board[row][k] == 0)
-                            {
-                                tokens[row][k].setFill(Color.RED);
-                                board[row][k] = 2;
-                                valid = true;
-                                break;
+                        // NOT VALID MOVE
+                        if (!gameOver)
+                        {    
+                            if(board[t.row][0] != 0) 
+                            {   
+                                displayMessage.setText("The column is full. Try again!");
+                                return;
                             }
-                        }
-                    } while(!valid);
-                });
+                            // PLAY PLAYERS MOVE
+                            for(int k=board[t.row].length-1; k>=0; --k)
+                            {
+                                if(board[t.row][k] == 0)
+                                {
+                                    tokens[t.row][k].setFill(Color.YELLOW);
+                                    board[t.row][k] = 1;
+                                    break;
+                                }
+                            }
+                            // PLAY COMPUTER'S MOVE
+                            int row;
+                            boolean valid = false;
+                            do
+                            {
+                                row = rand.nextInt(board.length);
+                                for(int k=board[row].length-1; k>=0; --k)
+                                {
+                                    if(board[row][k] == 0)
+                                    {
+                                        tokens[row][k].setFill(Color.RED);
+                                        board[row][k] = 2;
+                                        valid = true;
+                                        displayMessage.setText("The computer played in column " + (row + 1));
+                                        break;
+                                    }
+                                }
+                            } while(!valid);
+
+                            for (int k = 0; k < board.length; k++)
+                            {
+                                if (board[k][0] == 0)
+                                    break;
+                                if (k == board.length - 1 && board[k][0] != 0)
+                                {
+                                    gameOver = true;
+                                    displayMessage.setText(displayMessage.getText() + " and tied!");
+                                }
+                            }
+                            if (!gameOver)
+                                displayMessage.setText(displayMessage.getText() + ". Your turn!");
+                        }});
             }
             // Create Bottom Number
             Label nLabel = new Label((i+1) + "");
@@ -94,24 +116,24 @@ public class A4 extends Application
             pane.add(nLabel, i, board[0].length);
         }
         pane.setStyle("-fx-background-color: blue; -fx-margin: 50%;");
-        // FOR SOME REASON commenting this line out fixes background?.....
-        displayMessage = new Label("Hello guys");
+
+        displayMessage = new Label("It's your turn to play...");
+        displayMessage.setFont(Font.font("Comic Sans MS", 18));   
         displayMessage.setStyle("-fx-text-fill: aqua;");
-        //displayMessage.setAlignment(Pos.BASELINE_CENTER);
+
         pane.setHalignment(displayMessage, HPos.CENTER);
         pane.add(displayMessage, 0, board[0].length + 1, board.length, 1);
-        
-        
+
         int w = (cellSize+padding) * board.length + padding;
         int h = (int)((cellSize+padding) * (board[0].length + .5))    + padding;
         h += cellSize;
         Scene scene = new Scene(pane, w, h, Color.BLUE);
         stage.setTitle("Connect 4 Game");
         stage.setScene(scene);
-        
+
         stage.show();
     }
-    
+
     public class Token extends Circle
     {
         int row, col;
@@ -120,7 +142,7 @@ public class A4 extends Application
             super((cellSize)/2);
             this.row = row;
             this.col = col;
-            
+
             setFill(Color.WHITE);
         }
     }
